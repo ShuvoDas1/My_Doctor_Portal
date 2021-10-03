83bfe16ff96d3d7f5761649e9b322b47e079b63f
Shuvo Das <shuvo0381@gmail.com>
2021-10-03 19:17:54 +0600

    add client server

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const  MongoClient  = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+const port = 4000
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vktpy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err => {
+  const appointmentCollection = client.db("newDoctorsPortal").collection("appointments");
+  
+  app.post('/addAppointment',(req,res)=>{
+    const appointment = req.body;
+    //console.log(appointment)
+    appointmentCollection.insertOne(appointment)
+    .then(result=>{
+      res.send(result)
+    })
+  })
+
+  app.post('/appointmentsByDate', (req, res)=>{
+    const date= req.body;
+    appointmentCollection.find({date: date.date})
+    .toArray((err,documents)=>{
+        res.send(documents)
+    })
+  })
+
+
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || port)
